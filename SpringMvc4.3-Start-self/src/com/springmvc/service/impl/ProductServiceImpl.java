a256d8d85f7ead25dc770e587750d9b51a738266
wangyuchen <chen_841460210@qq.com>
2017-02-21 13:49:02 +0800

    add SpringMVC Signed-off-by: wangyuchen <chen_841460210@qq.com>

--- SpringMvc4.3-Start-self/src/com/springmvc/service/impl/ProductServiceImpl.java
+++ SpringMvc4.3-Start-self/src/com/springmvc/service/impl/ProductServiceImpl.java
@@ -10,14 +10,15 @@ import org.springframework.stereotype.Service;
 
 import com.springmvc.domain.Product;
 import com.springmvc.service.ProductService;
+
 @Service
-public class ProductServiceImpl implements ProductService{
-	private Map<Long, Product> products=new HashMap<Long,Product>();//数据库的数据保存与获取
+public class ProductServiceImpl implements ProductService {
+	private Map<Long, Product> products = new HashMap<Long, Product>();// 数据库的数据保存与获取
 	private AtomicLong generator = new AtomicLong();
-	private List<Product> pList=new ArrayList<Product>();
-	
-	public ProductServiceImpl(){
-		Product product =new Product();
+	private List<Product> pList = new ArrayList<Product>();
+
+	public ProductServiceImpl() {
+		Product product = new Product();
 		product.setName("MiBook");
 		product.setDescription("MiBook write code is very good!");
 		product.setPrice(4999.0d);
@@ -26,36 +27,37 @@ public class ProductServiceImpl implements ProductService{
 		add(product);
 	}
 	@Override
-	public Product add(Product product){
-		long newid=generator.incrementAndGet();
+	public Product add(Product product) {
+		long newid = generator.incrementAndGet();
 		product.setId(newid);
-		
-		//保存到MAP中，模拟数据保存到数据库
+		// 保存到MAP中，模拟数据保存到数据库
 		products.put(newid, product);
 		pList.add(product);
 		return product;
 	}
+
 	@Override
 	public Product get(long id) {
 		return products.get(id);
 	}
+
 	@Override
 	public List<Product> show() {
-		
 		return pList;
 	}
+
 	@Override
-	public List<Product> modify(long id,Product product) {
-		int Id=(int)id-1;
+	public List<Product> modify(long id, Product product) {
+		int Id = (int) id - 1;
 		pList.remove(pList.get(Id));
 		pList.add(Id, product);
 		return pList;
 	}
+
 	@Override
 	public List<Product> delete(long id) {
-		int Id=(int)id-1;
+		int Id = (int) id - 1;
 		pList.remove(pList.get(Id));
 		return pList;
 	}
-	
 }
